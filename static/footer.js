c817d65bc67da6723eb3a09e3bd7e16fd5987b44
vasu-h10 <your_github_email@example.com>
2025-10-27 15:38:42 +0530

    Added footer.js to complete layout rendering

---
+++ static/footer.js
@@ -0,0 +1,7 @@
+// footer.js
+export function renderFooter() {
+  const footer = document.getElementById("app-footer");
+  footer.innerHTML = `
+    <p>© 2025 My Vindhu · All rights reserved</p>
+  `;
+}
